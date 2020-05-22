55d17c317969ebffc16773313082e739cb030330
Zainab Saud <zai99nb@gmail.com>
2020-05-22 17:32:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Write your code here !
+
+let ele = document.getElementById("main");
+ele.remove();
+
+
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeaderText = document.createTextNode("YOUR-NAME is the champion");
+newHeader.innerHTML="<h1>is the champion</h1>";
