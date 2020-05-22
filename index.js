f7ea6840be2a7bc2b931eb014b3987094ccc2550
Zainab Saud <zai99nb@gmail.com>
2020-05-22 17:36:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Write your code here !
+
+let ele = document.getElementById("main");
+ele.remove();
+
+
+
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML="YOUR-NAME is the champion";
+
