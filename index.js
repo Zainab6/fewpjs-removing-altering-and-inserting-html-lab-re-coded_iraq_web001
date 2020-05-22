2381bb3180630c53ef685ef6d94978d7d22a0bc5
Zainab Saud <zai99nb@gmail.com>
2020-05-22 17:35:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ ele.remove();
 
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.innerHTML=("YOUR-NAME is the champion");
-newHeader.innerHTML="<h1>is the champion</h1>";
+newHeader.innerHTML="YOUR-NAME is the champion";
+
